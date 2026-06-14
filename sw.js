e14a3b7bf13f044a6c7837e37e7b7d53b242dc3b
mugikohaone-netizen <mugikoha.one@gmail.com>
2026-06-14 23:00:02 +0900

    🔄 SW更新 vmqdupr1z

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-mqdu7k90';
+const CACHE = 'app-mqdupr1z';
 const FILES = ['./','./index.html'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()));
